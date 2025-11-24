3f169a165d181f51a8e257c5c984308b2065d700
alejandrorubioo-hue <alejandro.rubioo@uniagustiniana.edu.co>
2025-11-24 23:38:54 +0000

    ajustenextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "randomuser.me",
-      // agrega aquí otros dominios si usas más imágenes externas
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/**",
+      },
+    
     ],
   },
-  // ...otras opciones de configuración si tienes
+
 };
 
 export default nextConfig;
